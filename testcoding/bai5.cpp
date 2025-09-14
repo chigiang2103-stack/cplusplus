57e5d1020ce41633bd3eea6ceb5210b215b23383
chigiang2103-stack <chigiang2103@gmail.com>
2025-09-14 23:54:49 +0700

    added

--- testcoding/bai5.cpp
+++ testcoding/bai5.cpp
@@ -1,36 +1,24 @@
 #include<iostream> 
 using namespace std;
+#define ll long long 
 int main () {
-    int n ;
+    ll n ;
     cout <<""; cin >>n;
-    int find(int n );
-    int res=n;
-    bool flag = true ;
-    for (int i = n/2 +1 ; i <n ; i++) {
+    ll find(ll n );
+    ll res;
+    for (int i = n - 100 ; i < n ; i++) {
         if (find(i)==n) {
             res = i;
-            flag = true ;
             break;
         }
-        else {
-            flag = false;
-        }
-    } 
-    if (flag) {
-        cout << res <<endl;
     }
-    else {
-        cout <<"false" <<endl;
-    }
-    
-
+    cout << res <<endl;
 }
-int find(int n ) {
-     int temp = n , res =temp;
+ll find(ll n ) {
+    ll temp = n , res =temp;
     while (temp > 0) {
         res +=temp%10;
         temp = temp/10;
     }
     return res;
 }
-
