e9d86aa0ef3dc3db9e890c7eeb97fd2884389f0a
chigiang2103-stack <chigiang2103@gmail.com>
2025-09-14 23:54:07 +0700

    added

--- testcoding/bai4.cpp
+++ testcoding/bai4.cpp
@@ -1,25 +1,24 @@
 #include<iostream> 
 using namespace std;
 int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n ;
     cout <<""; cin >>n;
-    int count,temp;
-    // 18 = 3x6 = 5x3 +3x1 (dc chọn 3 or 5)
-    // 21 = 3.7 = 5.3 + 3.2
-    // n = 3.a + 5.b => a + b min 
-    if (n%3!=0 && n%5 !=0 ) {
-        cout <<-1;
-    }
-    else {
-        for (int i = n/5 ; i>=0 ; i--) {
-            temp = n - i*5;
-            if (temp%3==0) {
-                count = i + temp/3;
-                break;
+    int count=0;
+    bool flag = true; 
+    for (int i = n/5 ; i>=0 ; i--) {
+        int temp = n - i*5;
+        if (temp%3==0) {
+            count = i + temp/3;
+            flag = true;
+            break;
             }
-
+        else {
+            flag = false;
         }
     }
-    cout << count <<endl;
-
+    if (flag) cout << count << endl;
+    else cout <<-1<<endl;
+    return 0;
 }
